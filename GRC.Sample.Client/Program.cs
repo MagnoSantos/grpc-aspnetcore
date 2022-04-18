780a23f99fcba1af76c0392de0daa00d57a69672
Magno Santos <magno.ufvjm@gmail.com>
2022-04-18 14:46:15 -0300

    feat: refactor

--- GRC.Sample.Client/Program.cs
+++ GRC.Sample.Client/Program.cs
@@ -1,28 +1,3 @@
-﻿using GRC.Sample;
-using Grpc.Core;
-using Grpc.Net.Client;
-using Grpc.Net.Client.Configuration;
+﻿using GRPC.Sample.Client.Services;
 
-var defaultMethodConfig = new MethodConfig
-{
-    Names = { MethodName.Default },
-    RetryPolicy = new RetryPolicy
-    {
-        MaxAttempts = 3,
-        InitialBackoff = TimeSpan.FromSeconds(1),
-        MaxBackoff = TimeSpan.FromSeconds(5),
-        BackoffMultiplier = 1.5,
-        RetryableStatusCodes = { StatusCode.Unavailable }
-    }
-};
-
-var channel = GrpcChannel.ForAddress("https://localhost:7135", new GrpcChannelOptions
-{
-    ServiceConfig = new ServiceConfig { MethodConfigs = { defaultMethodConfig } }
-});
-
-var client = new Greeter.GreeterClient(channel);
-
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "grpc demonstration" });
-
-Console.WriteLine(@"Reply: {reply}", reply);
+await DefaultCallgRPCService.CallAsync();
---
+++ GRC.Sample.Client/Services/DefaultCallgRPCService.cs
@@ -0,0 +1,36 @@
+﻿using GRC.Sample;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Configuration;
+
+namespace GRPC.Sample.Client.Services;
+
+public class DefaultCallgRPCService
+{
+    public static async Task CallAsync()
+    {
+        var defaultMethodConfig = new MethodConfig
+        {
+            Names = { MethodName.Default },
+            RetryPolicy = new RetryPolicy
+            {
+                MaxAttempts = 3,
+                InitialBackoff = TimeSpan.FromSeconds(1),
+                MaxBackoff = TimeSpan.FromSeconds(5),
+                BackoffMultiplier = 1.5,
+                RetryableStatusCodes = { StatusCode.Unavailable }
+            }
+        };
+
+        var channel = GrpcChannel.ForAddress("https://localhost:7135", new GrpcChannelOptions
+        {
+            ServiceConfig = new ServiceConfig { MethodConfigs = { defaultMethodConfig } }
+        });
+
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "grpc demonstration" });
+
+        Console.WriteLine(@"Reply: {reply}", reply);
+    }
+}
